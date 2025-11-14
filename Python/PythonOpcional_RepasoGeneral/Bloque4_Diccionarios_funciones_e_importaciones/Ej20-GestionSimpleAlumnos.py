47a0f27f223abd8e6b6fee49896a0145a1ba092e
ErikCampusFP <erik.pizarrosanabria.23@campusfp.es>
2025-11-14 20:09:38 +0100

    Update Ej20-GestionSimpleAlumnos.py

--- Python/PythonOpcional_RepasoGeneral/Bloque4_Diccionarios_funciones_e_importaciones/Ej20-GestionSimpleAlumnos.py
+++ Python/PythonOpcional_RepasoGeneral/Bloque4_Diccionarios_funciones_e_importaciones/Ej20-GestionSimpleAlumnos.py
@@ -27,25 +27,66 @@ for i in menu:
 eleccion = int(input("Introduce una opción: "))
 while eleccion != 5:
     match eleccion:
+        # Añadimos un alumno
         case 1:
             nombre = input("Introduce un nombre: ")
+            while nombre in alumnado:
+                print("Nombre ya existente")
+                nombre = input("Introduce un nombre: ")
+
             nota = int(input("Introduce una nota: "))
             while nota < 0 or nota > 10:
                 print("Nota no valida")
                 nota = int(input("Introduce una nota: "))
             alumnado.update({nombre: nota})
+            print("Alumno Añadido Correctamente")
+
+        # Consultamos la nota de un alumno
         case 2:
             alumnoEnlista = False
             nombreAlumno = input("Introduce el nombre del alumno: ")
             for i in alumnado:
                 precio = alumnado[i]
                 if i == nombreAlumno:
-
                     print(f"La nota de {nombreAlumno} es {precio}")
                     alumnoEnlista = True
             if alumnoEnlista == False:
                 print(f"No hay ningun {nombreAlumno} en el diccionario")
+            else:
+                print("Alumno generado correctamente")
+        
+        # Modificamos la nota de un alumno existente
+        case 3:
+            alumnoEnlista2 = False 
+            nombreAlumno2 = input("Introduce el nombre del alumno: ")
+            for i in alumnado:
+                precio = alumnado[i]
+                if i == nombreAlumno2:
+                    nuevaNota = int(input("Introduce una nota: "))
+                    while nuevaNota < 0 or nuevaNota > 10:
+                        print("Nota no valida")
+                        nuevaNota = int(input("Introduce una nota :"))
+                    alumnado.update({i: nuevaNota })
+                    nombreAlumno2 = True
+            
+            if nombreAlumno2 == True:
+                print("Nota cambiada correctamente")
+            else:
+                print("Alumno no valido")
+        
+        # Mostramos todos los alumnos
+        case 4:
+            for i in alumnado:
+                precio = alumnado[i]
+                print(f"{i}: {precio}")
+        # Salimos
+        case 5:
+            eleccion = 5
+        # Casos no validos
+        case _:
+            print("Opción no valida")
                 
     eleccion = int(input("Introduce una opción: "))
 
 
+print("Hasta Luego")
