5e5f3868e5c8e6ce426d790dc1ce97eb7c51cfd6
ErikCampusFP <erik.pizarrosanabria.23@campusfp.es>
2025-12-17 10:27:15 +0100

    Finalización Ejercicios 1 PHP

--- php/work/PHP_Ejercicios1_ErikPizarroSanabria/Ej3_LaTiendaDeFruta/formulario.php
+++ php/work/PHP_Ejercicios1_ErikPizarroSanabria/Ej3_LaTiendaDeFruta/formulario.php
@@ -1,12 +1,17 @@
 <?php
 // Productos
-$productos = ["Manzana" => 1.5, "Sandia" => 2.5, "Platano" => 2.0];
+$productos = ["Manzana" => 1.5, "Sandia" => 2.5, "Platano" => 2.0, "Pera" => 3];
 
+// Generamos el formulario
+echo "<form action='procesoFormulario.php' method='POST'>";
+
+// Generamos un label e imput por producto
 foreach ($productos as $nombre => $precio) {
-      echo "$nombre - $precio €: ";
-      echo '<input type="number" name="cantidad_' . strtolower($nombre) . '" min="0" value="0">';
-      echo "<br><br>";
-  }
+  echo "<label>$nombre</label>". "   ";
+  echo "<input type='number' name='$nombre' min='0' required><br><br>";
+}
+
+echo "<input type='submit' value='Enviar'>";
+echo "</form>";
 
 ?>
-
