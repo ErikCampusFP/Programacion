6758ac29f34d72012cfd6bc072e5cf27a7f28604
ErikCampusFP <erik.pizarrosanabria.23@campusfp.es>
2026-04-13 19:18:57 +0200

    Actividad_Evaluable_Actualización

--- Java/ActividadEvaluable1_ErikPizarroSanabria/src/Empleado.java
+++ Java/ActividadEvaluable1_ErikPizarroSanabria/src/Empleado.java
@@ -1,4 +1,67 @@
 
 public class Empleado {
+	private int id;
+	private String nombre;
+	private String puesto;
+	private String email;
+	private String telefono;
+	
+	public Empleado(int id, String nombre, String puesto, String email, String telefono) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.puesto = puesto;
+		this.email = email;
+		this.telefono = telefono;
+	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getPuesto() {
+		return puesto;
+	}
+
+	public void setPuesto(String puesto) {
+		this.puesto = puesto;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	@Override
+	public String toString() {		
+		return "Empleado [id=" + id + ", nombre=" + nombre + ", puesto=" + puesto + ", email=" + email + ", telefono="
+				+ telefono + "]";
+	}
+	
+	
+	
+	
 }
--- Java/ActividadEvaluable1_ErikPizarroSanabria/src/Tarea.java
+++ Java/ActividadEvaluable1_ErikPizarroSanabria/src/Tarea.java
@@ -1,4 +1,84 @@
 
 public class Tarea {
+	private int id;
+	private String nombre;
+	private String dificultad;
+	private String descripcion;
+	
+	public Tarea(int id, String nombre, String dificultad, String descripcion) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.dificultad = dificultad;
+		this.descripcion = descripcion;
+	}
+	
+	
+	
 
+	public int getId() {
+		return id;
+	}
+
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+
+
+	public String getDificultad() {
+		return dificultad;
+	}
+
+
+
+
+	public void setDificultad(String dificultad) {
+		this.dificultad = dificultad;
+	}
+
+
+
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+
+
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+
+
+
+	@Override
+	public String toString() {
+		return "Tarea [id=" + id + ", nombre=" + nombre + ", dificultad=" + dificultad + ", descripcion=" + descripcion
+				+ "]";
+	}
+	
+	
+	
+	
 }
