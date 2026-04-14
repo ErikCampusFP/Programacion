55baa2802d84ed4b28112aa536245fa1d59f44d9
ErikCampusFP <erik.pizarrosanabria.23@campusfp.es>
2026-04-14 19:37:12 +0200

    Actualización ActividadEvaluable

--- Java/ActividadEvaluable1_ErikPizarroSanabria/src/Tarea.java
+++ Java/ActividadEvaluable1_ErikPizarroSanabria/src/Tarea.java
@@ -4,23 +4,32 @@ public class Tarea {
 	private String nombre;
 	private String dificultad;
 	private String descripcion;
-	
-	public Tarea(int id, String nombre, String dificultad, String descripcion) {
+    private Empleado empleadoAsignado;
+    private boolean completada;
+    
+    /**
+     * @param id
+     * @param nombre
+     * @param dificultad
+     * @param descripcion
+     * @param empleadoAsignado
+     * @param completada
+     */
+	// Constructor 1 -> Básico
+	public Tarea(int id, String nombre, String dificultad, String descripcion, Empleado empleadoAsignado,
+			boolean completada) {
 		super();
 		this.id = id;
 		this.nombre = nombre;
 		this.dificultad = dificultad;
 		this.descripcion = descripcion;
+		this.empleadoAsignado = empleadoAsignado;
+		this.completada = completada;
 	}
-	
-	
-	
 
 	public int getId() {
 		return id;
 	}
-
-
 
 
 	public void setId(int id) {
@@ -28,13 +37,9 @@ public class Tarea {
 	}
 
 
-
-
 	public String getNombre() {
 		return nombre;
 	}
-
-
 
 
 	public void setNombre(String nombre) {
@@ -42,13 +47,9 @@ public class Tarea {
 	}
 
 
-
-
 	public String getDificultad() {
 		return dificultad;
 	}
-
-
 
 
 	public void setDificultad(String dificultad) {
@@ -56,13 +57,9 @@ public class Tarea {
 	}
 
 
-
-
 	public String getDescripcion() {
 		return descripcion;
 	}
-
-
 
 
 	public void setDescripcion(String descripcion) {
@@ -70,13 +67,38 @@ public class Tarea {
 	}
 
 
+	public Empleado getEmpleadoAsignado() {
+		return empleadoAsignado;
+	}
+
+
+	public void setEmpleadoAsignado(Empleado empleadoAsignado) {
+		this.empleadoAsignado = empleadoAsignado;
+	}
+
+
+	public boolean isCompletada() {
+		return completada;
+	}
+
+
+	public void setCompletada(boolean completada) {
+		this.completada = completada;
+	}
 
 
 	@Override
 	public String toString() {
 		return "Tarea [id=" + id + ", nombre=" + nombre + ", dificultad=" + dificultad + ", descripcion=" + descripcion
-				+ "]";
+				+ ", empleadoAsignado=" + empleadoAsignado + ", completada=" + completada + "]";
 	}
+	
+	
+	
+    
+	
+	
+
 	
 	
 	
