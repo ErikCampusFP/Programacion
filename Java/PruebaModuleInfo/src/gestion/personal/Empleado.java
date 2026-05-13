45676396eaad6240bc7a8efc8d8ca98995fcd840
ErikCampusFP <erik.pizarrosanabria.23@campusfp.es>
2026-05-13 09:46:51 +0200

    Programación Entero

---
+++ Java/PruebaModuleInfo/src/gestion/personal/Empleado.java
@@ -0,0 +1,13 @@
+package gestion.personal;
+public class Empleado {
+   private String nombre;
+   private double salario;
+   public Empleado(String nombre, double salario) {
+       this.nombre = nombre;
+       this.salario = salario;
+   }
+   @Override
+   public String toString() {
+       return "Empleado [nombre=" + nombre + ", salario=" + salario + "]";
+   }
+}
---
+++ Java/PruebaModulo2/src/module-info.java
@@ -0,0 +1,3 @@
+module miaplicacion {
+	requires empresa;
+}
